e1c52088a286f10d89491017ca6899b67c75a3cf
Alikson2025 <lawalibrahimanas34@gmail.com>
2025-02-03 05:31:52 -0800

    First commit

---
+++ Balkisu-Ibrahim-Sanda-C++/Balkisu-Ibrahim-Sanda-C++.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Display personal information
+    cout << "Name: Balkisu Ibrahim Sanda" << endl;
+    cout << "Email: balkisuibrahimsanda@gmail.com" << endl;
+    cout << "Phone Number: +1234567890" << endl;
+    cout << "Area of Interest: Software Development" << endl;
+
+    return 0;
+}
