90000ae31cfcf0ebea197e5d9152d8403afa8833
Alikson2025 <lawalibrahimanas34@gmail.com>
2025-02-03 06:00:04 -0800

    First commit

---
+++ Alhaji-Hassan-Hashimu-R/Alhaji-Hassan-Hashimu-R.R
@@ -0,0 +1,11 @@
+# Define the details
+name <- "Alhaji Hassan Hashimu"
+email <- "alhajihassanhashimu@gmail.com"
+phone_number <- "+2348069219966"
+area_of_interest <- "Bioinformatics tool development"
+
+# Display the details
+cat("Name:", name, "\n")
+cat("Email:", email, "\n")
+cat("Phone Number:", phone_number, "\n")
+cat("Area of Interest:", area_of_interest, "\n")
