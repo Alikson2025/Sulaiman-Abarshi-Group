fa6245061f886ab4f5f34007f670111bfa2c7c92
Alikson2025 <lawalibrahimanas34@gmail.com>
2025-02-03 05:02:33 -0800

    First commit

---
+++ Hassana_Muhammad_Sale_Python/Hassana_Muhammad_Sale.py
@@ -0,0 +1,13 @@
+def display_personal_info():
+    name = "Hassana Muhammad Sale"
+    email = "muhammadsalehhassana@gmail.com"
+    phone_number = "+1234567890"
+    area_of_interest = "Software Development"
+
+    print("Name:", name)
+    print("Email:", email)
+    print("Phone Number:", phone_number)
+    print("Area of Interest:", area_of_interest)
+
+if __name__ == "__main__":
+    display_personal_info()
