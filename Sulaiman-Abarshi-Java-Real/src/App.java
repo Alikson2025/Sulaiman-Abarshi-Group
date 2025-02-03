59ed62484f40473f8fb1a400a321f5d32272b655
Alikson2025 <lawalibrahimanas34@gmail.com>
2025-02-03 04:21:19 -0800

    First commit

---
+++ Sulaiman-Abarshi-Java-Real/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String name = "Sulaiman Abarshi";
+        String email = "sulaimanabarshi5@gmail.com";
+        String phone = "09049452727";
+        String area_of_interest = "Sulaiman Abarshi";
+
+        System.out.println("Name"  + name);
+        System.out.println("Email:" + email);
+        System.out.println("Phone number:" + phone);
+        System.out.println("Area of interest:" + String area_of_interest);
+    }
+}
