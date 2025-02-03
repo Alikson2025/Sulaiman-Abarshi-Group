7505b8ac9d27ba97620e52a80dee4cf21fe84925
Alikson2025 <lawalibrahimanas34@gmail.com>
2025-02-03 06:08:29 -0800

    First commit

---
+++ Adamu-Usman-Mai-Perl/Adamu-Usman-Mai-Perl.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+# Define user information
+my $name = "Adamu Usman Mai";
+my $email = "adamuusmanmaimsa@gmail.com";
+my $phone_number = "+2347049947841";
+my $area_of_interest = "Structural Bioinformatics";
+
+# Display user information
+print "Name: $name\n";
+print "Email: $email\n";
+print "Phone Number: $phone_number\n";
+print "Area of Interest: $area_of_interest\n";
